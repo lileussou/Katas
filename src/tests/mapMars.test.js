9735ff7b0ae11a1f880aff6ce1e2092fde8f2cca
lileussou <cameron.cisse@live.fr>
2023-10-11 16:15:26 +0200

    map mars test 2

--- src/tests/mapMars.test.js
+++ src/tests/mapMars.test.js
@@ -12,4 +12,15 @@ describe ('Test the current map size', () => {
         //Then
         expect(returnedMap).toMatchObject(expectedMap);
     }); 
+
+    it('Given x = 5, y = 5 return "{longitudeSize : 5, latitudeSize:5)', () => {
+        //Given
+        const x = 5;
+        const y = 5;
+        const expectedMap = new Map(x, y);
+        //When
+        const returnedMap = getMap(x,y);
+        //Then
+        expect(returnedMap).toMatchObject(expectedMap);
+    }); 
 });
