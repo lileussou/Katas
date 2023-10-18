52151acc532be3731fd36d9f19cc68ec7ddef949
lileussou <cameron.cisse@live.fr>
2023-10-18 14:44:26 +0200

    rovermars angle ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useCallback, useState } from 'react';
 
 import Rover from './objects/rover';
 import Map from './objects/map';
-import {changeRoverPosition} from './controller/roverMars';
+import {
+  changeRoverPosition,
+  angleBasedOnDirection,
+} from './controller/roverMars';
 import {Directions} from './models/directions';
 import {Commands} from './models/commands';
 import roverMarsImg from './image/roverMarsImg.png';
@@ -16,14 +19,13 @@ import './App.css';
 function App() {
 
   const mapMars = new Map(4,4);
-  const style = {
-    transform: 'rotate(90deg)',
-  }
   const [roverMars, setRoverMars] = useState(new Rover(1, 1, Directions.N));
+  const [angle, setAngle] = useState(0);
   
   const moveRover = useCallback((command) => {
     const newRover = changeRoverPosition(roverMars, [command]);
     setRoverMars(new Rover(newRover.longitude, newRover.latitude, newRover.facing));
+    setAngle(angleBasedOnDirection(newRover.facing));
   }, [roverMars]);
 
   return (
@@ -36,7 +38,7 @@ function App() {
                 <div key = {`${longitudeIndex}, ${latitudeIndex}`} className='longitude' id= {`${longitudeIndex}, ${latitudeIndex}`}>
                   {longitudeElement === roverMars.longitude
                     && latitudeElement === roverMars.latitude ?
-                      <img alt='roverMarsImg' src={roverMarsImg} className='mapMarsImg' style={style} />
+                      <img alt='roverMarsImg' src={roverMarsImg} className='mapMarsImg' style={{ transform: `rotate(${angle}deg)`}} />
                       :
                       <img alt='mapMarsImg' src={mapMarsImg} className='mapMarsImg'/>
                   } 
